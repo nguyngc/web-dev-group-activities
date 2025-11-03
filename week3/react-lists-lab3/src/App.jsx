009f51c8399912e907b160a7636cbbd25c6a76cf
Ngoc Nguyen <ngoc.nguyen15@metropolia.fi>
2025-11-03 09:52:56 +0200

    Add react actitities - week 3

---
+++ week3/react-lists-lab3/src/App.jsx
@@ -0,0 +1,13 @@
+import TourList from "./TourList";
+import { tours } from "./toursData";
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <TourList tours={tours} />
+    </main>
+  );
+}
+
+export default App;
