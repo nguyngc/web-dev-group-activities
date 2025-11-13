979adf8f41bae092939edffebccb960ee6d58b64
Ngoc Nguyen <ngoc.nguyen15@metropolia.fi>
2025-11-13 05:18:19 +0200

    Add BE Group Activity Week 4

---
+++ week4/week4-be-activity2/controllers/blogController.js
@@ -0,0 +1,105 @@
+const Blog = require("../models/blogModel");
+
+// GET /blogs
+const getAllBlogs = async (req, res) => {
+  try {
+    const blogs = await Blog.find({}).sort({ createdAt: -1 });
+    res.status(200).json(blogs);
+  } catch (error) {
+    res.status(500).json({ message: "Fail to retrieve blogs" });
+  }
+};
+
+// POST /blogs
+const createBlog = async (req, res) => {
+  try {
+    const newBlog = await Blog.create({ ...req.body });
+    res.status(201).json(newBlog);
+  } catch (error) {
+    res.status(400).json({ message: "Failed to create blog", error: error.message });
+  }
+};
+
+// GET /blogs/:blogId
+const getBlogById = async (req, res) => {
+  const { blogId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(blogId)) {
+    return res.status(400).json({ message: "Invalid blog ID" });
+  }
+
+  try {
+    const blog = await Blog.findById(blogId);
+    if (blog) {
+      res.status(200).json(blog);
+    } else {
+      res.status(404).json({ message: "Blog not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Failed to retrieve blog" });
+  }
+};
+
+// PUT /blogs/:blogId
+const updateBlog = async (req, res) => {
+  const { blogId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(blogId)) {
+    return res.status(400).json({ message: "Invalid blog ID" });
+  }
+
+  try {
+    // Prevent user from sending _id in request body
+    const { _id, ...safeData } = req.body;
+    const updatedBlog = await Blog.findOneAndReplace(
+      { _id: blogId },
+      safeData,
+      {
+        new: true,
+        runValidators: true, // validate against schema before replacing
+        overwrite: true// ensures full replace instead of partial update
+      }
+    );
+
+    // const updatedBlog = await Blog.findOneAndUpdate(
+    //   { _id: blogId },
+    //   { ...req.body },
+    //   { new: true }
+    // );
+    if (updatedBlog) {
+      res.status(200).json(updatedBlog);
+    } else {
+      res.status(404).json({ message: "Blog not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Failed to update blog" });
+  }
+};
+
+// DELETE /blogs/:blogId
+const deleteBlog = async (req, res) => {
+  const { blogId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(blogId)) {
+    return res.status(400).json({ message: "Invalid blog ID" });
+  }
+
+  try {
+    const deletedBlog = await Blog.findOneAndDelete({ _id: blogId });
+    if (deletedBlog) {
+      res.status(200).json({ message: "Blog deleted successfully" });
+    } else {
+      res.status(404).json({ message: "Blog not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Failed to delete blog" });
+  }
+};
+
+module.exports = {
+  getAllBlogs,
+  getBlogById,
+  createBlog,
+  updateBlog,
+  deleteBlog,
+};
